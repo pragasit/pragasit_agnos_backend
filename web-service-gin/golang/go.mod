7351cc8f6f9736a056a781787aba7cb2c1126684
pragasit <32674358+pragasit@users.noreply.github.com>
2023-10-27 20:03:57 +0700

    update

--- web-service-gin/golang/go.mod
+++ web-service-gin/golang/go.mod
@@ -2,10 +2,7 @@ module example/web-service-gin
 
 go 1.20
 
-require (
-	github.com/gin-gonic/gin v1.9.1
-	github.com/jmoiron/sqlx v1.3.5
-)
+require github.com/gin-gonic/gin v1.9.1
 
 require (
 	github.com/bytedance/sonic v1.10.2 // indirect
@@ -20,7 +17,6 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
-	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
--- web-service-gin/golang/main.go
+++ web-service-gin/golang/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"example/web-service-gin/handlers"
-	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"unicode"
-
-	"internal/database"
-	"smalldoc124/service/cmd/sales-api/handlers"
 
 	"github.com/gin-gonic/gin"
 )
@@ -101,36 +95,4 @@ func main() {
 	router.POST("/strong_password_steps", password)
 	router.Run("localhost:8080")
 
-	var port, dbhost, dbschema, dbusername, dbpassword, disableTLS string
-	var dbport int
-	flag.StringVar(&port, "port", "3000", "port for open service")
-	flag.StringVar(&dbhost, "dbhost", "localhost", "database host name")
-	flag.IntVar(&dbport, "dbport", 5432, "database port")
-	flag.StringVar(&dbschema, "dbschema", "smalldoc", "database schema name")
-	flag.StringVar(&dbusername, "dbusername", "smalldoc", "database user name")
-	flag.StringVar(&dbpassword, "dbpassword", "example", "database password")
-	flag.StringVar(&disableTLS, "disableTLS", "Y", "database disableTLS[Y/n]")
-	flag.Parse()
-	var databaseTSL bool
-	if disableTLS == "n" {
-		databaseTSL = false
-	} else {
-		databaseTSL = true
-	}
-	dbConfig := database.Config{
-		User:       dbusername,
-		Password:   dbpassword,
-		Host:       dbhost,
-		Port:       dbport,
-		Name:       dbschema,
-		DisableTLS: databaseTSL,
-	}
-
-	db, err := database.Open(dbConfig)
-	if err != nil {
-		log.Fatal("connecting database fail", err)
-	}
-	engine := gin.Default()
-	handlers.API(db, engine)
-	log.Fatal(engine.Run(fmt.Sprintf(":%s", port)))
 }
