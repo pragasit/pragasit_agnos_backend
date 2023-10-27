072a3528281dab778e1b6649d475207a8ced55fb
pragasit <32674358+pragasit@users.noreply.github.com>
2023-10-27 23:28:59 +0700

    update

---
+++ web-service-gin/main_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+// func Test_password(t *testing.T) {
+// 	// type args struct {
+// 	// 	c *gin.Context
+// 	// }
+// 	// tests := []struct {
+// 	// 	name string
+// 	// 	args args
+// 	// }{}
+// 	// for _, tt := range tests {
+// 	// 	t.Run(tt.name, func(t *testing.T) {
+// 	// 		password(tt.args.c)
+// 	// 	})
+// 	// }
+// 	newPassword := main.requestStruct{
+// 		pass: "aA1",
+// 	}
+// 	newPasswordJSON, _ := json.Marshal(newPassword)
+
+// 	request := httptest.NewRequest("POST", "/strong_password_steps", bytes.NewBuffer(newPasswordJSON))
+// 	writer := httptest.NewRecorder()
+
+//		testRoute := gin.Default()
+//		testRoute.POST("/strong_password_steps", password)
+//		testRoute.ServeHTTP(writer, request)
+//	}
+func TestIntMinTableDriven(t testing.T) {
+	var tests = []struct {
+		Init_password string
+		want          int
+	}{
+		{"aA1", 3},
+		{"1445D1cd", 0},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%d,%d", tt.Init_password, tt.want)
+		t.Run(testname, func(ttesting.T) {
+			ans := password(tt.Init_password)
+			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+}
+func BenchmarkIntMin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IntMin(1, 2)
+	}
+}
